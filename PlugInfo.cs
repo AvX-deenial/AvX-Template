bd1d4853edebaec304187f1f4cc0ae1e74341b1b
AvX <159103591+AvX-deenial@users.noreply.github.com>
2025-03-17 12:18:46 -0700

    Add files via upload

---
+++ PlugInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace AVTemp
+{
+    internal class PlugInfo
+    {
+        public const string GUID = "org.legal.gorillatag.modtemplate";
+        public const string Name = "AvX";
+        public const string Description = "Created by av with unity";
+        public const string Version = "1.0.0";
+    }
+}
